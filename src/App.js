a5e95889c39cf2f8236c46ccc8dea2b8d6471f20
Netah Eliezerov <65071328+NetahEliezerov@users.noreply.github.com>
2023-10-10 21:30:37 +0300

    probably actually final

--- src/App.js
+++ src/App.js
@@ -7,31 +7,39 @@ import { useEffect, useState } from "react";
 const timeline = [
   {day: "6", time: -7150, type: "normal", description: 'Music festival begins, an event that takes place once a year and is considered to be one of the most important electronic music communities in Israel'},
 
-  {day: "7", time: 0, type: "normal", description: '"Simchat Torah", one of the holiest Jewish days on the calendar.'},
+  {day: "7", time: 0, type: "normal", description: '"Simchat Torah", one of the holiest Jewish days'},
   
-  {day: "7", time: 5000, type: "normal", description: "Since it's establishment in 87', Hamas has carried out countless terrorist attacks against israeli civilians."},
+  {day: "7", time: 5000, type: "normal", description: "More than 3,000 Hamas terrorists are getting armed and preparing rockets"},
   
-  {day: "7", time: 10000, type: "normal", description: "Hamas is recognized as a terror organiation by the United States, United Kingdom, Canada, Japan, Australia, and the European union"},
+  {day: "7", time: 10000, type: "normal", description: "4,000 Young Israelis celebrate in a nature rave party"},
   
-  {day: "7", time: 23640, type: "normal", description: '3 Million people wake up to 5,000 rockets shot at Israel, No one knows why.'},
+  {day: "7", time: 15640, type: "normal", description: 'Hundreds of Israeli families are sleeping in Kibutzs not far from the Gaza border'},
   
-  {day: "7", time: 24300, type: "normal", description: '1,000 terrorists invade into israel'},
+  {day: "7", time: 23640, type: "normal", description: '3 Million people wake up to 1,000 rockets shot at Israel'},
+  
+  {day: "7", time: 24300, type: "normal", description: '1,500 terrorists invade into israel through the Gaza border'},
 
-  {day: "7", time: 25200, type: "normal", description: 'Terrorists are taking control of dozens of cities and human settlements. Terrorists are getting into the Israeli party, and starts to mass-shoot at over 500 citizens'},
+  {day: "7", time: 26500, type: "normal", description: 'Terrorists are pretending to be IDF soliders, and knock on people’s door. As they open the door, they’re getting shot'},
 
-  {day: "7", time: 25500, type: "normal", description: 'Terrorists are pretending to be IDF soliders, and knock on people’s door. As they open the door, they’re getting killed.'},
+  {day: "7", time: 28200, type: "normal", description: '500 Hundreds grandparents, mothers, fathers and children are slaughtered'},
+  
+  {day: "7", time: 29700, type: "normal", description: 'Others hide in shelters but get burnt'},
+  
+  {day: "7", time: 30700, type: "normal", description: 'Around 100 families are dragged into pickup trucks. Fathers are shot, Mothers, childrens and toddlers and even a few babies are taken'},
+  
+  {day: "7", time: 31700, type: "normal", description: '150 Terrorists are getting into the party and mass-shoot killing over 260 ravers, wounding hundreds of others while they escape'},
 
-  {day: "7", time: 26100, type: "normal", description: 'Hundreds of people were taken hostage by the terrorists, Terrorists are starting to shoot at citizens.'},
+  {day: "7", time: 32700, type: "normal", description: '30 girls and boys are taken violently into Gaza'},
 
-  {day: "7", time: 26700, type: "normal", description: 'Families starting to hide in their protected rooms, praying to not getting killed.'},
+  {day: "7", time: 33700, type: "normal", description: 'Additional 4,000 rockets are shot into main cities across Israel, attempting to kill as many civillians as possible'},
   
-  {day: "7", time: 27200, type: "normal", description: 'Citizens are praying for themselves, terrorists mass shooting across Southern Israel, as Hamas are shooting bombs nonstop across Israel'},
+  {day: "7", time: 34700, type: "normal", description: 'Citizens are praying for themselves and for their nation'},
   
-  {day: "7", time: 27800, type: "normal", description: 'This is what happened in the first hour of the morning.'},
+  {day: "7", time: 35700, type: "normal", description: 'This is what happened in the first hour of the morning'},
   
-  {day: "7", time: 28200, type: "question", description: 'Do you really support a terror organization?'},
+  {day: "7", time: 36700, type: "question", description: 'Do you support these horrible actions?'},
   
-  {day: "7", time: 30200, type: "buttons", description: null},
+  {day: "7", time: 41700, type: "buttons", description: null},
 ]
 
 function App() {
@@ -39,7 +47,7 @@ function App() {
   const date = Date.now() + 3603; // 1 hour & 3 seconds from now for demo
   const [seconds, setTimer] = useState(-7150);
   const [currentTimerlineObject, setCurrentTimelineObject] = useState(timeline[0]);
-  const [scrollMultiplier, setScrollMultiplier] = useState(3);
+  const [scrollMultiplier, setScrollMultiplier] = useState(10);
   const [descriptionSize, setDescriptionSize] = useState(null);
   const [scrollPosition, setScrollPosition] = useState(200);
   const [showDesc, setShowDesc] = useState(true);
@@ -78,7 +86,7 @@ function App() {
           setCurrentTimelineObject(e);
           setTimeout(() => setShowDesc(true), 600);
           if(e.type == "question") {
-            setDescriptionSize((position - 11784.2421875) / 16 + 32);
+            setDescriptionSize((position - 4403.2001953125) / 16 + 32);
           } else {
             setDescriptionSize(null);
           }
@@ -105,10 +113,10 @@ function App() {
 
   return (
     <div className="App">
-    <h1 className='title'>Stand With Israel</h1>
+    <h1 className='title'>October 7, Morning Timeline</h1>
       <div className='main'>
         {/* <p style={{color: "white"}}>{seconds}</p> */}
-        <TimerComponent result={result} day={day} />
+        <TimerComponent result={result} day={day} />{window.outerWidth < 600 ? <div><br/><br/><br/><br/><br/><br/></div> : <></> }
         <DescriptionComponent size={descriptionSize} showDesc={showDesc} content={currentTimerlineObject} />
       </div>
     </div>
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const DescriptionComponent = ({content, showDesc, size}) => {
-  const currentStyle = size != null ? { fontSize: `${Math.round(size)}px`, color: "red", opacity: `${(Math.round(window.pageYOffset*3) - 7150 - 28171) / 988 + 0.3}` } : {};
+  const currentStyle = size != null ? { fontSize: `${Math.round(size)}px`, color: "red", opacity: `${(Math.round(window.pageYOffset*3) - 7150 - 4403.2001953125) / 4403.2001953125}` } : {};
 
   return (
     <div style={{ width: "77vw", marginTop: "-10vh", float: 'right' }}>
       {content.type != "buttons" ? <p
       className={`descriptionText ${!showDesc ? "noOpacity" : ""}`}
       style={currentStyle}>{content.description}</p> : <div>
-        <a className='TwitterBtn' href="https://twitter.com/intent/tweet?text= &hashtags=IStandWithIsrael">
+        <a className='TwitterBtn' href="https://twitter.com/intent/tweet?text= &hashtags=HamasWarCrimes">
           <img style={{ height: '90.5px', width: '315px', margin: '55px', animationName: 'examplefull', animationDuration: '600ms' }} src='/twitterBtn.png' />
         </a>
       </div>}
